63d4bc241dfe14d98821dc2a2aded558007bb812
IceEspresso <jwoo5264@gmail.com>
2022-01-04 16:05:43 +0900

    봇 불러오기 내보내기

--- index.py
+++ index.py
@@ -14,4 +14,24 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=None)
 
 
+@bot.command()
+async def join(ctx):
+    try:
+        global vc
+        vc = await ctx.message.author.voice.channel.connect()
+    except:
+        try:  # 유저가 접속하지 않았다면 다른 채널에는 없는지 확인한 다음에 있다면 이동(기존사용하던것을 뺏는다)
+            await vc.move_to(ctx.message.author.voice.channel)
+        except:
+            await ctx.send("There is no user in this Channel")
+
+
+@bot.command()
+async def out(ctx):
+    try:
+        await vc.disconnect()
+    except:
+        await ctx.send("It is not belonging this channel")
+
+
 bot.run(token)
